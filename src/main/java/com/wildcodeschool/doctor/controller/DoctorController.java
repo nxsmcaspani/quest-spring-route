644e4915e57516ee76d0d4b272b2edf1ba7ce615
Maxime Caspani <maxime.caspani@natixis.com>
2021-05-25 10:18:31 +0200

    Quest 556 completed

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,27 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
-
-        return null;
+    public Doctor doctor(@PathVariable int number) {
+        if (number < 13 ) {
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER);
+        } else if (number == 13){
+            Doctor Jodie = new Doctor(13, "Jodie Whittaker");
+            return Jodie;
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Cannot retrieve Doctors' incarnation number "+number);
+        }
     }
 }
